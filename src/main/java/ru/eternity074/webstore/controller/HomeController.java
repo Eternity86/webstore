48a13185ce37bd1209398469fab8ebcb647893fa
AK <AK@LAPTOP-R6BEK1CM>
2020-03-29 18:29:00 +0300

    default request mapping method

--- src/main/java/ru/eternity074/webstore/controller/HomeController.java
+++ src/main/java/ru/eternity074/webstore/controller/HomeController.java
@@ -3,11 +3,13 @@ package ru.eternity074.webstore.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 	
-	@GetMapping("/")
+	@GetMapping
 	public String welcome(Model model) {
 		model.addAttribute("greeting", "Welcome to Web Store!");
 		model.addAttribute("tagline", "The one and only amazing web store");
