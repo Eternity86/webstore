372bf559256da1be873a941ce06932ed32576c43
AK <AK@LAPTOP-R6BEK1CM>
2020-03-29 22:30:11 +0300

    serving static resources

--- src/main/java/ru/eternity074/webstore/config/WebAppConfig.java
+++ src/main/java/ru/eternity074/webstore/config/WebAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -37,5 +38,10 @@ public class WebAppConfig implements WebMvcConfigurer {
 		resolver.setSuffix(".jsp");
 
 		return resolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/img/**").addResourceLocations("/resources/images/");
 	}
 }
