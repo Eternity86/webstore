ff440ce920d7e9ca4168da79bff6d6369f582159
AK <AK@LAPTOP-R6BEK1CM>
2020-03-29 18:24:47 +0300

    adding class-level request mapping

--- src/main/java/ru/eternity074/webstore/controller/ProductController.java
+++ src/main/java/ru/eternity074/webstore/controller/ProductController.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import ru.eternity074.webstore.service.ProductService;
 
 @Controller
+@RequestMapping("market")
 public class ProductController {
 
 	@Autowired
@@ -24,7 +26,7 @@ public class ProductController {
 	public String updateStock(Model model) {
 		productService.updateAllStock();
 
-		return "redirect:/products";
+		return "redirect:/market/products";
 	}
 
 }
